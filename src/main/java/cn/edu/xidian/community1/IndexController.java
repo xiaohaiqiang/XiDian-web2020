a5d515667e7610d5f71b3ad7c132b91c81df1130
xiaohaiqiang <297978747@qq.com>
2020-03-29 18:16:12 +0800

    add index and Bootstrap

---
+++ src/main/java/cn/edu/xidian/community1/IndexController.java
@@ -0,0 +1,13 @@
+package cn.edu.xidian.community1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
