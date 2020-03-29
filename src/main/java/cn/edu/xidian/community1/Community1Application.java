96a90e3a333c665fe85c53ee0014d900118277aa
xiaohaiqiang <297978747@qq.com>
2020-03-29 14:54:45 +0800

    first commit

---
+++ src/main/java/cn/edu/xidian/community1/Community1Application.java
@@ -0,0 +1,13 @@
+package cn.edu.xidian.community1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community1Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/xidian/community1/Community1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xidian.community1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Community1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
